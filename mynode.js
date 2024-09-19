bc6318359ea87a4ed02f4bdb83a867d45058f9c6
Leonardo Amorim <leonardo.amorim@gomara.tech>
2024-09-19 13:33:10 -0300

    fix

--- mynode.js
+++ mynode.js
@@ -8,12 +8,12 @@ const envFile = `export const environment = {
     APIURL: '${process.env.APIURL}'
 };
 `;
-const targetPath = path.join(__dirname, './src/environments/environment.development.ts');
+const targetPath = path.join(__dirname, './src/environments/environment.ts');
 fs.writeFile(targetPath, envFile, (err) => {
     if (err) {
         console.error(err);
         throw err;
     } else {
-        console.log(successColor, `${checkSign} Successfully generated environment.development.ts`);
+        console.log(successColor, `${checkSign} Successfully generated environment.ts`);
     }
 });
